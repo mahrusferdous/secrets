4476a78bde30048bea34aa2f5c03205f1bbec7a6
mahrusferdous <65342347+mahrusferdous@users.noreply.github.com>
2020-07-18 20:13:09 -0400

    Allow users to submit a secret

--- app.js
+++ app.js
@@ -36,7 +36,8 @@ const userSchema = new mongoose.Schema({
   //Google
   googleId: String,
   //Facebook
-  facebookId: String
+  facebookId: String,
+  secret: String
 })
 
 userSchema.plugin(passportLocalMongoose)
@@ -123,11 +124,40 @@ app.get("/register", function(req, res){
 })
 
 app.get("/secrets", function(req, res){
+  User.find({"secret": {$ne: null}}, function(err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        res.render("secrets", {usersWithSecrets: foundUser})
+      }
+    }
+  })
+})
+
+app.get("/submit", function(req, res){
   if (req.isAuthenticated()) {
-    res.render("secrets")
+    res.render("submit")
   } else {
     res.redirect("/login")
   }
+})
+
+app.post("/submit", function(req, res){
+  const submittedSecret = req.body.secret
+
+  User.findById(req.user.id, function(err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        foundUser.secret = submittedSecret
+        foundUser.save(function(){
+          res.redirect("/secrets")
+        })
+      }
+    }
+  })
 })
 
 app.get("/logout", function(req, res){
